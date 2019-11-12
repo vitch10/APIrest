ccece53a3fafaf571539212b03b0a18c2633a4a6
Dona Esther Ludmila Vihozounkou <dona.esther.ludmila.vihozounkou@ibm.com>
2019-11-12 17:52:07 -0300

    Project Finished

---
+++ lib/app.ts
@@ -0,0 +1,33 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Routes } from "./routes/Routes";
+import * as mongoose from "mongoose";
+
+class App {
+
+    public app: express.Application = express();
+    public routePrv: Routes = new Routes();
+    // public mongoUrl: mongodb+srv://gildo10:<password>@api-ycsg1.mongodb.net/test
+    public mongoUrl: string = 'mongodb+srv://gildo10:87654321@api-ycsg1.mongodb.net/test';
+
+    constructor() {
+        this.config();
+        this.mongoSetup();
+        this.routePrv.routes(this.app);     
+    }
+
+    private config(): void{
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        
+        
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl, {useNewUrlParser: true});
+        console.log('Banco de Dados Connectado');       
+    }
+
+}
+export default new App().app;
---
+++ lib/routes/Routes.ts
@@ -0,0 +1,53 @@
+import {Request, Response} from "express";
+
+export class Routes {    
+    
+    public routes(app): void {   
+        
+        app.route('/')
+        .get((req: Request, res: Response) => {            
+            res.status(200).send({
+                message: ' GET Gildo esta trabalhando'
+            })
+        })
+        
+        // Contact 
+        app.route('/contact') 
+        // GET endpoint 
+        .get((req: Request, res: Response) => {
+        // Get all contacts            
+            res.status(200).send({
+                message: 'GET Gildo esta comendo'
+            })
+        })        
+        // POST endpoint
+        .post((req: Request, res: Response) => {   
+        // Create new contact         
+            res.status(200).send({
+                message: 'POST request successfulll!!!!'
+            })
+        })
+
+        // Contact detail
+        app.route('/contact/:contactId')
+        // get specific contact
+        .get((req: Request, res: Response) => {
+        // Get a single contact detail            
+            res.status(200).send({
+                message: 'GET Gildo e Menucha'
+            })
+        })
+        .put((req: Request, res: Response) => {
+        // Update a contact           
+            res.status(200).send({
+                message: 'PUT request successfulll!!!!'
+            })
+        })
+        .delete((req: Request, res: Response) => {       
+        // Delete a contact     
+            res.status(200).send({
+                message: 'DELETE request successfulll!!!!'
+            })
+        })
+    }
+}
